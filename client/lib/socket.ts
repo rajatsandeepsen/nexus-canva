f8d64c734285395a21ba23d1fc9f8085a0b9241e
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-11-12 22:21:24 +0530

    env change next public

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -2,7 +2,7 @@ import { io } from 'socket.io-client'
 
 const SERVER =
   process.env.NODE_ENV === 'production'
-    ? process.env?.SOCKET_URL
+    ? process.env?.NEXT_PUBLIC_SOCKET_URL
     : 'http://localhost:3001'
 
 if (!SERVER) throw new Error('Missing env vars for socket.io');
