6cf6cbf80d0b0553d0bf94f7cc4fa8e14b4c3e92
Rajat Sandeep <93584596+rajatsandeepsen@users.noreply.github.com>
2023-11-13 05:31:57 +0530

    fix: env

--- client/lib/socket.ts
+++ client/lib/socket.ts
@@ -1,9 +1,6 @@
 import { io } from 'socket.io-client'
 
-const SERVER =
-  process.env.NODE_ENV === 'production'
-    ? process.env?.NEXT_PUBLIC_SOCKET_URL
-    : 'http://localhost:3001'
+const SERVER = process.env.NEXT_PUBLIC_SOCKET_URL || 'http://localhost:3001'
 
 if (!SERVER) throw new Error('Missing env vars for socket.io');
 
